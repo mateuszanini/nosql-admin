31d3a3a9c3a63443ccb114a811b7f51d00ad46ee
Mateus Zanini <mateus-zanini@hotmail.com>
2017-04-10 12:45:28 +0000

    Iniciando template de usuário/empresa

--- app/assets/js/script.js
+++ app/assets/js/script.js
@@ -9,10 +9,21 @@ var app = {
             closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
             draggable: true // Choose whether you can drag to open on touch screens
         });
-        $('.button-collapse').sideNav('show');
+        //$('.button-collapse').sideNav('show');
 
         $('#modalRecuperarSenha').modal({
             dismissible: true
+        });
+
+        $(".dropdown-button").dropdown({
+            hover: true
+        });
+        
+        $(document).ready(function() {
+            $('.tooltipped').tooltip({
+                delay: 50,
+                position: 'top'
+            });
         });
 
         // Initialize Firebase
